d061624eb1ab224bed94d1e96f51e89c9765f3cc
vijaydanielm <vijaydanielm@yahoo.com>
2014-09-19 21:15:45 +0530

    Slight change to navbar

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -4,7 +4,9 @@
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span
                     class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span
                     class="icon-bar"></span></button>
-            <a class="navbar-brand" href="{{{ URL::to('/') }}}">Clinic In The Sky</a>
+            <a class="navbar-brand" href="{{{ URL::to('/') }}}">
+                Clinic In The Sky
+            </a>
         </div>
 
         <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1">
