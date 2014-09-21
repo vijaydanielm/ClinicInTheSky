0e120733d25d40f90735638116e4bed562684521
vijaydanielm <vijaydanielm@yahoo.com>
2014-09-21 22:58:06 +0530

    Adding view helper with changed case

---
+++ app/ViewHelpers/ValidationError.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VijayDaniel
+ * Date: 9/21/14
+ * Time: 9:36 PM
+ */
+
+namespace ViewHelpers;
+
+
+class ValidationError {
+
+    /**
+     * @param array $validationErrors
+     * @param       $fieldName
+     * @return string
+     */
+    public static function feedback(array $validationErrors = null, $fieldName) {
+
+        if(!is_null($validationErrors) and
+           array_key_exists($fieldName, $validationErrors) and $validationErrors[$fieldName]
+        ) {
+
+            return " has-error has-feedback";
+        }
+
+        return "";
+    }
+} 
